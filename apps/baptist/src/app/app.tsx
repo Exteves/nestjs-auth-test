dec795bf7e4a5a02c083484acad756bcee72ced7
Renan <mirandarenan.e@gmail.com>
2023-03-26 11:09:17 -0300

    feat: adding tailwind

--- apps/baptist/src/app/app.tsx
+++ apps/baptist/src/app/app.tsx
@@ -1,46 +1,19 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Header from './header/header';
+import { Login } from './login/login';
 
 export function App() {
   return (
     <>
-      <div />
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
+      <Header></Header>
       <Routes>
         <Route
-          path="/"
+          path="/login"
           element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <p>second page</p>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
+            <Login></Login>
           }
         />
       </Routes>
-      {/* END: routes */}
     </>
   );
 }
